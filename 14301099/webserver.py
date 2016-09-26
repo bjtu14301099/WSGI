6467c2f389692a8b3a1f823ba0567515f8054c06
bjtu14301099 <14301099@bjtu.edu.cn>
2016-09-26 23:43:22 +0800

    Update webserver.py

--- 14301099/webserver.py
+++ 14301099/webserver.py
@@ -138,7 +138,7 @@ def make_server(server_address, application):
 
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    body = '<h1>Die~Die~Die~  Hello,%s!</h1>' % (environ['PATH_INFO'][1:] or 'web')	
+    body = '<h1>Hello,%s!</h1>' % (environ['PATH_INFO'][1:] or 'web')	
     return [body.encode('utf-8')]    
 
 
